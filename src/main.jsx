1c84fe16a3d3f7ea74ea408ace12cee3b7947dde
Andrea Khan <khan.andrea@gmail.com>
2026-02-18 20:52:38 -0500

    added initial storyblok setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { apiPlugin, storyblokInit } from "@storyblok/react";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+
+const storyblokToken = import.meta.env.VITE_STORYBLOK_TOKEN;
+
+storyblokInit({
+  accessToken: storyblokToken,
+  use: [apiPlugin],
+  apiOptions: {
+    region: "eu",
+  },
+  components: {},
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
